5fc6f4fb827f3c7e86960feb2a8c6ea18da4b780
DarknessKiller <23330152+DarknessKiller@users.noreply.github.com>
2021-04-16 10:41:27 +0800

    sdm660-common: init: SafetyNet Workaround
    * Switch to walleye oreo build fingerprint and set ro.boot.verifiedbootstate to green
    * Set Bootloader & VBMeta as Locked
    * Set Verity to Enforcing
    * Fix My Mess
    Signed-off-by: DarknessKiller <23330152+DarknessKiller@users.noreply.github.com>

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -107,4 +107,14 @@ void vendor_load_properties()
     property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_override("dalvik.vm.heapminfree", heapminfree);
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+
+   // Safetynet Workaround
+    property_override("ro.boot.flash.locked", "1");
+    property_override("ro.boot.verifiedbootstate", "green");
+    property_override("ro.boot.veritymode", "enforcing");
+    property_override("ro.boot.vbmeta.device_state", "locked");
+    property_override("ro.build.description", "walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys");
+    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    property_override("ro.system.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+
 }
